e8bb271c2b228e001cd373c65edcd6d71ce6077b
Garfield Lee <Garfield550@users.noreply.github.com>
2025-01-01 21:12:46 +0800

    feat: use local eslint-parser-plain package

--- packages/eslint-plugin-format/src/index.ts
+++ packages/eslint-plugin-format/src/index.ts
@@ -1,7 +1,6 @@
 import type { ESLint, Linter } from 'eslint'
 
-import { parseForESLint } from 'eslint-parser-plain'
-import { getPackageInfoSync } from 'local-pkg'
+import parserPlain from '@bttin/eslint-parser-plain'
 
 import packagePlugin from '../package.json'
 import dprint from './rules/dprint.js'
@@ -9,16 +8,6 @@ import prettier from './rules/prettier.js'
 
 interface PluginWithParser extends ESLint.Plugin {
   parserPlain: Linter.Parser
-}
-
-const packageParserPlain = getPackageInfoSync('eslint-parser-plain')
-
-const parserPlain: Linter.Parser = {
-  meta: {
-    name: packageParserPlain?.name,
-    version: packageParserPlain?.version,
-  },
-  parseForESLint,
 }
 
 export default {
