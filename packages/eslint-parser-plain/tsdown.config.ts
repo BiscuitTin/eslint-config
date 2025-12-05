a109530cf74e7df3365c448cdd7e86704190d5ed
Garfield Lee <Garfield550@users.noreply.github.com>
2025-12-05 14:24:37 +0800

    chore: disable attw for eslint-plugin-format and eslint-parser-plain

--- packages/eslint-parser-plain/tsdown.config.ts
+++ packages/eslint-parser-plain/tsdown.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsdown/config'
 
 export default defineConfig({
   entry: 'src/index.ts',
-  attw: true,
+  attw: false,
   clean: true,
   dts: true,
   tsconfig: '../../tsconfigs/tsconfig.default.json',
