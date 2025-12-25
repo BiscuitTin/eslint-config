0e1e00107600baa48a5aea5c653b5102f591b448
Garfield Lee <Garfield550@users.noreply.github.com>
2025-12-25 11:40:52 +0800

    feat: change jsx-shorthand-boolean rule to never (#296)

--- packages/eslint-config/src/configs/react.ts
+++ packages/eslint-config/src/configs/react.ts
@@ -213,7 +213,8 @@ const configStylistic: TypedFlatConfigItem = {
     '@eslint-react/prefer-destructuring-assignment': 'error',
     '@eslint-react/prefer-namespace-import': 'warn',
     '@eslint-react/jsx-shorthand-boolean': 'error',
-    '@eslint-react/jsx-shorthand-fragment': 'off',
+    // @ts-expect-error https://www.eslint-react.xyz/docs/rules/jsx-shorthand-fragment
+    '@eslint-react/jsx-shorthand-fragment': -1,
     '@eslint-react/naming-convention/filename': 'off', // ['error', 'kebab-case'], already handled by unicorn
 
     // @stylistic/eslint-plugin
